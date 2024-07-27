06599bf54ac95fa8fbabc7c372c4ba0a15cdccbe
Krzysztof Kadela <krzysztofkadela01@gmail.com>
2024-07-27 16:22:48 +0000

    Add functions update_stock for update values for stock goods in and out.

--- run.py
+++ run.py
@@ -3,6 +3,7 @@
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 import gspread
 from google.oauth2.service_account import Credentials
+import pandas as pd
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -17,9 +18,17 @@ SHEET = GSPREAD_CLIENT.open('NO_Stock')
 
 products = SHEET.worksheet('Product List')
 
-data = products.get_all_values()
+#Geting all values in the worksheet as DataFrame
+data = products.get_all_records()
+df = pd.DataFrame(data)
+# Creating a list of products from the "Product Name" column.
+product_list = df['Product Name'].to_list()
 
-print(data)
+print(product_list)
+
+#data_1 = products.get_all_values()
+
+#print(data_1)
 
 def main_menu():
     """
@@ -33,7 +42,7 @@ def main_menu():
     print("2 - Updated product list")
     print("3 - Production Report's")
     print("4 - Finsh")
-    user_choice = input("Whats you want to do?")
+    user_choice = input("Whats you want to do \n?")
     return user_choice
 
 def update_stock_menu():
@@ -48,7 +57,7 @@ def update_stock_menu():
     print("3 - Stock correction.")
     print("4 - Stock Take")
     print("5 - Return to main menu.")
-    user_choice = input("Whats you want to do?")
+    user_choice = input("Whats you want to do?\n")
     return user_choice
 
 
@@ -68,7 +77,32 @@ def check_correct_input(choice, menu):
         except ValueError as e:
             print("uppsss")
 
+def upddate_stock(choice, product_list):
+    """
+    Updating stock, all option goods in goods out and corrections. 
+    """
+    if choice == "goods_out":
+        data = ()
+        for product in product_list:
+            while True:
+                  # asking user for correct input
+                  print("Please enter a numeric value for all products quantity.")
+                  value = input(f"How many of {num[0]} you wants to add to the stock?: ")
+                  try:
+                     # Attempt to convert the input to a float to handle numbers with decimals
+                     numeric_value = float(value)
+                     # If the conversion is successful, break the loop
+                     results += (product, numeric_value)
+                     break
+                  except ValueError:
+                     # If conversion fails, print an error message
+                     print("Wrong value. Please enter a valid number.")
+    return results
 
+        
+
+    
+    
 
 def main():
     """
@@ -87,10 +121,13 @@ def main():
              elif choice_I == '3':
                  print("You chose Choice 3.")
              elif choice_I == '4':
+                 print("You chose Choice 3.")
+             elif choice_I == '5':
                    print("Exiting the menu. Goodbye!")
                    break  # Exit the loop
              else:
                  print("Invalid choice. Please select a number between 1 and 4.")
+                 print("Please select on number from 1 to 4 and pres enter! ")
       elif choice == '2':
          print("You chose Choice 2.")
       elif choice == '3':
@@ -98,8 +135,9 @@ def main():
       elif choice == '4':
         print("Exiting the menu. Goodbye!")
         break  # Exit the loop
-    else:
-        print("Invalid choice. Please select a number between 1 and 4.")
+      else:
+         print("Invalid choice. Please select a number between 1 and 4.\n")
+         print("Please select one number from 1 to 4 and pres enter! ")
         
         
 
